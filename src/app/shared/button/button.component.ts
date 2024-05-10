453a13f98b4686e226fff8d5b932f19c4f214265
pedrodam99 <pedrodam99@gmail.com>
2024-05-10 20:17:58 -0300

    FEAT: Componente de botão

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() buttonLabel: string = '';
+  @Input() buttonColor: string = 'blue'
+  @Input() buttonIcon: string = '';
+
+  @Output() onClickEmitter: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  protected onClick(): void {
+    this.onClickEmitter.emit();
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
 export class SharedModule { }
